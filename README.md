cb0443de062d5a0f511b35fcda9ab25dde57d70e
Trevor Reed <reedtrev@gmail.com>
2025-02-03 10:23:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A collection of recordings I made to test the Retro Fighters BattlerGC Pro controller.
+
+Check the `captures/` folder for `.csv` and `.xlsx` files of the captures.
+
+Check the `graphs/` folder for simple plots of the data.
+
+Made in Godot 4.3 Stable.
---
+++ input_recorder.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var device: int = 0
+var output_file = FileAccess.open("output.txt", FileAccess.WRITE)
+@export var text: TextEdit
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+    text.text = "FPS %d" % Engine.get_frames_per_second()
+    var datum = "%f, %f\n" % [Time.get_unix_time_from_system(), Input.get_joy_axis(device, JOY_AXIS_TRIGGER_RIGHT)]
+    #var datum = "%f, %f\n" % [Time.get_unix_time_from_system(), Input.get_joy_axis(device, JOY_AXIS_TRIGGER_LEFT)]
+    output_file.store_string(datum)
+
+
+func _notification(what):
+    if what == NOTIFICATION_WM_CLOSE_REQUEST:
+        output_file.close()
+        get_tree().quit()
